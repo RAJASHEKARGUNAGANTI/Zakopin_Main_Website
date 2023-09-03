31edc9fe9678cebacd359eae2c24d6669fc4456d
RAJASHEKARGUNAGANTI <gunagantirajashekar@gmail.com>
2023-09-03 22:08:51 +0530

    cell phone media query is changed

--- src/components/SmallNav/SmallNav.jsx
+++ src/components/SmallNav/SmallNav.jsx
@@ -7,17 +7,19 @@ import {FaProjectDiagram} from 'react-icons/fa'
 import {FaUserGraduate} from 'react-icons/fa'
 import {BiMessageSquareDetail} from 'react-icons/bi'
 import { useState } from 'react'
+import { Link } from 'react-router-dom';
+
 
 const SmallNav = () => {
   const[activeNav, setActiveNav] = useState("#")
   return (
     <nav className='SmallNav'>
-      <a href="#" onClick={()=> setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><BiHomeAlt /></a>
-      <a href="#about" onClick={()=> setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><AiOutlineUser /></a>
-      <a href="/services" onClick={()=> setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><BiBook /></a>
-      <a href="#portfolio" onClick={()=> setActiveNav('#portfolio')} className={activeNav === '#portfolio' ? 'active' : ''}><FaProjectDiagram /></a>
-      <a href="#qualification"onClick={()=> setActiveNav('#qualification')} className={activeNav === '#qualification' ? 'active' : ''}><FaUserGraduate /></a>
-      <a href="#contact"onClick={()=> setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><BiMessageSquareDetail /></a>
+      <Link to="#" onClick={()=> setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><BiHomeAlt /></Link>
+      <Link to="#about" onClick={()=> setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><AiOutlineUser /></Link>
+      <Link to="/services" onClick={()=> setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><BiBook /></Link>
+      <Link to="#portfolio" onClick={()=> setActiveNav('#portfolio')} className={activeNav === '#portfolio' ? 'active' : ''}><FaProjectDiagram /></Link>
+      <Link to="#qualification"onClick={()=> setActiveNav('#qualification')} className={activeNav === '#qualification' ? 'active' : ''}><FaUserGraduate /></Link>
+      <Link to="#contact"onClick={()=> setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><BiMessageSquareDetail /></Link>
     </nav>
   )
 }
